656002e3dd92f070047a32afc79e3746ba084865
Francois Fouquet <fouquet.f@gmail.com>
2015-03-18 15:42:51 +0100

    major update

---
+++ org.kubi.model/metamodel/org.kubi.kubi.mm
@@ -0,0 +1,56 @@
+class org.kubi.Ecosystem {
+    @id
+    name : String
+    @contained
+    groupes : org.kubi.Group[0,*]
+    @contained
+    devices : org.kubi.Device[0,*]
+    @contained
+    technologies : org.kubi.Technology[0,*]
+}
+
+class org.kubi.Group {
+    @id
+    name : String
+}
+
+class org.kubi.Device {
+    @id
+    id : String
+
+    links : org.kubi.Device[0,*]
+    name : String
+    picture : String
+    version : String
+    manufacturer : String
+    technology : org.kubi.Technology
+    @contained
+    parameters : org.kubi.Parameter[0,*]
+    @contained
+    functions : org.kubi.Function[0,*]
+}
+
+class org.kubi.Parameter {
+    name : String
+    value : String
+    valueType : String
+    precision : Float
+    unit : String
+    range : String
+}
+
+class org.kubi.Function {
+    name : String
+    device : org.kubi.Device oppositeOf functions
+    @contained
+    returnType : org.kubi.Parameter
+    @contained
+    parameters : org.kubi.Parameter[0,*]
+    func exec(jsonInput : String) : String
+}
+
+class org.kubi.Technology {
+    @id
+    name : String
+    devices : org.kubi.Device[0,*] oppositeOf technology
+}

e6f349dbd4cf76093eb955129bb13864a5ad4e23
Jerome Rancati <jerome.rancati@gmail.com>
2015-08-03 09:59:46 +0200

    update view && delete web project

--- plugins/gui/org.kubi.plugins.polywebsite/src/main/resources/components/kubi/kubi-period-evolution/kubi-period-evolution.js
+++ plugins/gui/org.kubi.plugins.polywebsite/src/main/resources/components/kubi/kubi-period-evolution/kubi-period-evolution.js
@@ -55,12 +55,12 @@ function updatePeriodSettings(time, scale, devices){
 }
 
 function prettyDate(date) {
-    return  (date.getHours()<10?("0"+date.getHours()):date.getHours()) +":"
-        +   (date.getMinutes()<10?("0"+date.getMinutes()):date.getMinutes())+":"
-        +   (date.getSeconds()<10?("0"+date.getSeconds()):date.getSeconds())+"<br/>The "
-        +   (date.getDate()<10?("0"+date.getDate()):date.getDate())+"/"
-        +   ((date.getMonth()+1)<10?("0"+(date.getMonth()+1)):(date.getMonth()+1))+"/"
-        +   date.getFullYear();
+    return (date.getDate()<10?("0"+date.getDate()):date.getDate())+"/"
+    +   ((date.getMonth()+1)<10?("0"+(date.getMonth()+1)):(date.getMonth()+1))+"/"
+    +   (date.getFullYear()) +" At "
+    +   (date.getHours()<10?("0"+date.getHours()):date.getHours()) +":"
+    +   (date.getMinutes()<10?("0"+date.getMinutes()):date.getMinutes())+":"
+    +   (date.getSeconds()<10?("0"+date.getSeconds()):date.getSeconds());
 }
 
 /**

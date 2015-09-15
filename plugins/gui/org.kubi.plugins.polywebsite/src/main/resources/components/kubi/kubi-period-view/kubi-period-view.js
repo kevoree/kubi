a242e06e7b8d116771ee2b4a5d46a78f0584633e
Jerome Rancati <jerome.rancati@gmail.com>
2015-09-15 16:48:48 +0200

    fix

--- plugins/gui/org.kubi.plugins.polywebsite/src/main/resources/components/kubi/kubi-period-view/kubi-period-view.js
+++ plugins/gui/org.kubi.plugins.polywebsite/src/main/resources/components/kubi/kubi-period-view/kubi-period-view.js
@@ -34,13 +34,3 @@ function initTemplatePeriod(htmlIdSource, htmlIdTarget, universeTemplate, timeTe
         }
     });
 }
-
-paperclip.modifiers.timestampToDateConverter = function (timestamp){
-    var date = new Date(timestamp);
-    return (date.getDate()<10?("0"+date.getDate()):date.getDate())+"/"
-        +   ((date.getMonth()+1)<10?("0"+(date.getMonth()+1)):(date.getMonth()+1))+"/"
-        +   (date.getFullYear()) +" At "
-        +   (date.getHours()<10?("0"+date.getHours()):date.getHours()) +":"
-        +   (date.getMinutes()<10?("0"+date.getMinutes()):date.getMinutes())+":"
-        +   (date.getSeconds()<10?("0"+date.getSeconds()):date.getSeconds());
-}
